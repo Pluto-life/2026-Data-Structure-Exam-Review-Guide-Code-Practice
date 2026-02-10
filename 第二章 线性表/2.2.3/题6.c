ab4bb7667128f9453a2d900f2f7dc1cbd89df3f4
Pluto-life <plutolife@qq.com>
2026-02-10 16:41:54 +0800

    Update 题6.c

--- 第二章 线性表/2.2.3/题6.c
+++ 第二章 线性表/2.2.3/题6.c
@@ -4,23 +4,19 @@ typedef struct{
 	int length,listsize;
 }SqList;
 bool MergeList(SqList &La,SqList &Lb,SqList &Lc){
-	if (La.length+Lb.length>Lc.listsize) return false;
+	if (La.length+Lb.length>MAXSIZE) return false;
 	if (La.length==0) Lc=Lb;
 	if (Lb.length==0) Lc=La;
 	int i,j,k=0;
 	for (i=0,j=0;i<La.length&&j<Lb.length;){
-		if(La.data[i]<=Lb.data[j]){
-			Lc.data[k++]=La.data[i];
-			i++;}	
-		else{
-			Lc.data[k++]=Lb.data[j];
-			j++;}
+		if(La.data[i]<=Lb.data[j])
+			Lc.data[k++]=La.data[i++];
+		else Lc.data[k++]=Lb.data[j++];
 	}
-	if (i<La.length){
-		for (;i<La.length;i++)
-			Lc.data[k++]=La.data[i];}
-	if (j<Lb.length){
-		for (;j<Lb.length;j++)
-			Lc.data[k++]=Lb.data[j];}
+	while (i<La.length)
+		Lc.data[k++]=La.data[i];
+	while (j<Lb.length)
+		Lc.data[k++]=Lb.data[j];
+	Lc.length=k; 
 	return true;
 } 
