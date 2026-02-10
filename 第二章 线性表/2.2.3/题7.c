bfd8dc46dea5a3ca992953377be9b59bb829617e
Pluto-life <plutolife@qq.com>
2026-02-10 16:52:01 +0800

    Create 题7.c

---
+++ 第二章 线性表/2.2.3/题7.c
@@ -0,0 +1,21 @@
+#define MAXSIZE 50 
+typedef struct{
+	ElemType data[MAXSIZE];
+	int length,listsize;
+}SqList;
+void Reverse(ElemType A[],int left,int right,int arraySize){
+	if (left>=right||right>=arraySize) return;
+	ElemType temp;
+	while (left<right){
+        temp=A[left];
+        A[left]=A[right];
+        A[right]=temp;
+        left++;right--;
+    }
+}
+void Exchange(ElemType A[],int m,int n,int arraySize){
+	Reverse(A,0,m+n-1,arraySize);
+	Reverse(A,0,n-1,arraySize);
+	Reverse(A,n,m+n-1,arraySize);
+	
+}
