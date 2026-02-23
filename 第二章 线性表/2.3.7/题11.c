64662668c839b84edf21c85b7fcdcdc306afcac3
Pluto-life <plutolife@qq.com>
2026-02-23 21:08:11 +0800

    Create 题11.c

---
+++ 第二章 线性表/2.3.7/题11.c
@@ -0,0 +1,11 @@
+int Symmetry(DLinkList L){
+	DNode *p=L->next,*q=L->prior;
+	while(p!=q&&p->next!=q){
+		if(p->data==q->data){
+			p=p->next;
+			q=q->prior;
+		}
+		else return 0;
+	}
+	return 1;
+} 
