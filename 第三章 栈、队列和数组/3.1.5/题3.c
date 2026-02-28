4c2c9bb9f0e009d2b92b396ba0751090c9d8dcb3
Pluto-life <plutolife@qq.com>
2026-03-01 00:59:01 +0800

    Create 题3.c

---
+++ 第三章 栈、队列和数组/3.1.5/题3.c
@@ -0,0 +1,22 @@
+bool Judge(char A[]){
+	int i=0,j=0,k=0;
+	while(A[i]!='\0'){
+		switch(A[i]){
+			case 'I':j++;break;
+			case 'O':k++;
+			if(k>j){
+				printf("序列非法\n");
+				exit(0);
+			}
+		}
+		i++;
+	}
+	if(j!=k){
+		printf("序列非法\n");
+		return false;
+	}
+	else{
+		printf("序列合法\n");
+		return true; 
+	}
+}//Judge
