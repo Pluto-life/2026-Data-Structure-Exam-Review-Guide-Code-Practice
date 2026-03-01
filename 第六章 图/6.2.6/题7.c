0ff7248466f2a445f5de1eb3f16c1cde4c9baf66
Pluto-life <plutolife@qq.com>
2026-03-01 19:39:17 +0800

    Create 题7.c

---
+++ 第六章 图/6.2.6/题7.c
@@ -0,0 +1,17 @@
+typedef struct{
+	int numVertices,numEdges;
+	char VerticesList[MAXV];
+	int Edge[MAXV][MAXV];
+}MGraph;
+int IsExistEL(MGraph G){
+	int Degree[MAXV],degree=0,i,j,times=0;
+	for(i=0;i<MAXV;i++){
+		for(j=0;j<MAXV;j++)
+			if(G.Edge[i][j]!=0) degree++;
+	}
+	for(i=0;i<MAXC;i++){
+		if(Degree[i]%2==1) times++;
+	}
+	if(times==0||times=2) return 1;
+	else return 0;
+}
