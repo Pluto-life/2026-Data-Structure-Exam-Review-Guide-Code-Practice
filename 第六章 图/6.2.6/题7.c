1fef0cf588a13498d89a52e73b14d2436b416f65
Pluto-life <plutolife@qq.com>
2026-03-01 19:40:48 +0800

    Update 题7.c

--- 第六章 图/6.2.6/题7.c
+++ 第六章 图/6.2.6/题7.c
@@ -5,11 +5,11 @@ typedef struct{
 }MGraph;
 int IsExistEL(MGraph G){
 	int Degree[MAXV],degree=0,i,j,times=0;
-	for(i=0;i<MAXV;i++){
-		for(j=0;j<MAXV;j++)
+	for(i=0;i<G.numVertices;i++){
+		for(j=0;j<G.numVertices;j++)
 			if(G.Edge[i][j]!=0) degree++;
 	}
-	for(i=0;i<MAXC;i++){
+	for(i=0;i<G.numVertices;i++){
 		if(Degree[i]%2==1) times++;
 	}
 	if(times==0||times=2) return 1;
